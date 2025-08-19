cdc685703cdb58e88e8690af28d766c81bce86a5
scan2flip <scan2flip.dev@gmail.com>
2025-08-19 20:19:14 +0900

    Complete Scan2Flip app with mileage tracker and part-out calculator

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'scan2flip-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/scan2flip-logo.png',
+  '/icons/barcode-camera.png',
+  '/icons/inventory.png',
+  '/icons/dollar.png',
+  '/icons/powerscore.png',
+  '/icons/coins.png',
+  '/icons/mileage.png',
+  '/icons/goldmine.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
